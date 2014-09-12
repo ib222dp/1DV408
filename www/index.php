dbd4db0ec43ad1e4861ec8498813957aabd405b9
hl222ih <hl222ih@student.lnu.se>
2014-09-13 00:29:46 +0200

    html output based on example application

--- www/index.php
+++ www/index.php
@@ -1,6 +1,13 @@
 <?php
 require_once("views/HomeView.php");
 
+session_start();
+
+//if ($_POST) echo "this was a post";
+
 $homeView = new HomeView();
 
 $homeView->renderPage();
+
+//echo var_dump($_POST);
+//phpinfo();
--- www/views/HomeView.php
+++ www/views/HomeView.php
@@ -2,20 +2,42 @@
 
 class HomeView {
 
+    private $userName;
+    private $password;
+    private $autoLogin;
+
     public function renderPage() {
-        echo "
+        echo '
         <!DOCTYPE html>
         <html>
         <head>
-            <meta charset=\"utf-8\" />
+            <meta charset="utf-8" />
             <title>1DV408</title>
-            <link rel=\"stylesheet\" type=\"text/css\" href=\"styles/styles.css\" />
+            <link rel="stylesheet" type="text/css" href="styles/styles.css" />
         </head>
         <body>
-            hello world (åäö)
+            <h1>Laborationskod hl222ih</h1>
+            <p><a href="" onclick="alert(\'Saknar funktionalitet.\n\nFinns bara med för att de fanns med\n\npå bilderna i krav och testfall.\')">Registrera ny användare</a></p>
+            <h2>Ej Inloggad</h2>
+            <!--reset url-->
+            <form method="post">
+                <fieldset>
+                    <legend>Login - Skriv in användarnamn och lösenord</legend>
+                    <label for="userNameId">Användarnamn:</label>
+                    <input type="text" name="HomeView::UserName" id="userNameId"></input>
+                    <label for="passwordId">Lösenord:</label>
+                    <input type="password" name="HomeView::Password" id="passwordId"></input>
+                    <label for="autoLoginId">Håll mig inloggad:</label>
+                    <input type="checkbox" name="HomeView::AutoLoginChecked" id="autoLoginId"' .
+                        ($this->autoLogin ? "checked" : "")
+                        . '></input>
+                    <input type="submit" value="Logga in" />
+                </fieldset>
+            </form>
+        <p></p>
         </body>
         </html>
-        ";
+        ';
     }
 
 }

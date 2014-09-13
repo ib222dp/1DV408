fa9747596c89fad1401ef48062aae756e2df337c
hl222ih <hl222ih@student.lnu.se>
2014-09-13 03:41:14 +0200

    flytt setlocale till index

--- www/index.php
+++ www/index.php
@@ -3,6 +3,10 @@ require_once("views/HomeView.php");
 
 session_start();
 
+//sv_SE.UTF-8 funkar inte lokalt. sv och Swedish funkar, men kodningen blir fel.
+//Överallt verkar standarden språkkod_LANDSKOD.kodning gälla så jag kör på den. Har testat online på servern och det funkar där.
+setlocale(LC_TIME, 'sv_SE.UTF-8');
+
 //if ($_POST) echo "this was a post";
 
 $homeView = new HomeView();
--- www/views/HomeView.php
+++ www/views/HomeView.php
@@ -10,9 +10,6 @@ class HomeView {
     private $autoLogin;
 
     public function renderPage() {
-        //sv_SE.UTF-8 funkar inte lokalt. sv och Swedish funkar, men kodningen blir fel.
-        //Överallt verkar standarden språkkod_LANDSKOD.kodning gälla så jag kör på den. Har testat online på servern och det funkar där.
-        setlocale(LC_TIME, 'sv_SE.UTF-8');
         //Enligt exemplet stor bokstav på månad, men enl svenska skrivregler skrivs månaden med gemen initial, så tar friheten att ändra.
         $dateStr = sprintf("%s, den %d %s år %d. Klockan är [%s:%s:%s].", ucFirst(strftime("%A")) , date("j"), lcFirst(date("F")), date("Y"), date("H"), date("i"), date("s"));
         echo '

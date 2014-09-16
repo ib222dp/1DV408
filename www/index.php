88f3e8486d820cc06d24988924f63730e93ebe9e
hl222ih <hl222ih@student.lnu.se>
2014-09-16 06:37:26 +0200

    timezone Stockholm

--- www/index.php
+++ www/index.php
@@ -12,6 +12,7 @@ session_start();
 require_once("controllers/login-controller.php");
 
 setlocale(LC_TIME, 'sv_SE.UTF-8'); //problems on localhost but works on the server.
+date_default_timezone_set('Europe/Stockholm');
 
 $loginCtrl = new Ctrl\LoginController();
 $loginCtrl->start();

ab9d84f39f780f5867a506b0907f77ebbe18979e
ib222dp <ib222dp@users.noreply.github.com>
2014-10-05 17:09:49 +0200

    Update login-view.php

--- www/views/login-view.php
+++ www/views/login-view.php
@@ -1,52 +1,45 @@
 <?php
 
-namespace LoginApp\View;
-
-use LoginApp\Model\LoginModel;
-
 require_once("html-templates/head-html.php");
 require_once("html-templates/footer-html.php");
 
-
 abstract class LoginView {
 
-    protected $model;
-    protected $headHtml;
-    protected $footerHtml;
+	protected $model;
+	protected $headHtml;
+	protected $footerHtml;
 
-    protected $username;
-    protected $password;
-    protected $autoLogin;
+	protected $username;
+	protected $password;
+	protected $autoLogin;
 
-    protected $cookieUsernameKey;
-    protected $cookieEncryptedPasswordKey;
+	protected $cookieUsernameKey;
+	protected $cookieEncryptedPasswordKey;
 
-    public function __construct(LoginModel $model) {
-        $this->model = $model;
-        $this->footerHtml = new FooterHtml();
+	public function __construct(LoginModel $model) {
+		$this->model = $model;
+		$this->footerHtml = new FooterHtml();
 
-        $this->cookieUsernameKey = get_class() . '::UsernameKey';
-        $this->cookieEncryptedPasswordKey = get_class() . '::EncryptedPasswordKey';
-    }
+		$this->cookieUsernameKey = get_class() . '::UsernameKey';
+		$this->cookieEncryptedPasswordKey = get_class() . '::EncryptedPasswordKey';
+	}
 
-    public function redirectPage() {
-        header('location: ' . $_SERVER['PHP_SELF']);
-    }
+	public function redirectPage() {
+		header('location: ' . $_SERVER['PHP_SELF']);
+	}
 
-    public function getUsernameFromCookie() {
-        return (isset($_COOKIE[$this->cookieUsernameKey]) ? $_COOKIE[$this->cookieUsernameKey] : "");
-    }
+	public function getUsernameFromCookie() {
+		return (isset($_COOKIE[$this->cookieUsernameKey]) ? $_COOKIE[$this->cookieUsernameKey] : "");
+	}
 
-    public function getEncryptedPasswordFromCookie() {
-        return (isset($_COOKIE[$this->cookieEncryptedPasswordKey]) ? $_COOKIE[$this->cookieEncryptedPasswordKey] : "");
-    }
+	public function getEncryptedPasswordFromCookie() {
+		return (isset($_COOKIE[$this->cookieEncryptedPasswordKey]) ? $_COOKIE[$this->cookieEncryptedPasswordKey] : "");
+	}
 
-    public function clearCookies() {
-        unset($_COOKIE[$this->cookieEncryptedPasswordKey]);
-        setcookie($this->cookieEncryptedPasswordKey, null, -1, '/');
-        unset($_COOKIE[$this->cookieUsernameKey]);
-        setcookie($this->cookieUsernameKey, null, -1, '/');
-    }
-
-
+	public function clearCookies() {
+		unset($_COOKIE[$this->cookieEncryptedPasswordKey]);
+		setcookie($this->cookieEncryptedPasswordKey, null, -1, '/');
+		unset($_COOKIE[$this->cookieUsernameKey]);
+		setcookie($this->cookieUsernameKey, null, -1, '/');
+	}
 }

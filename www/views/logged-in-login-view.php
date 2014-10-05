e4478e6f7961c585064f6608621cfdda402728d2
ib222dp <ib222dp@users.noreply.github.com>
2014-10-05 17:08:22 +0200

    Update logged-in-login-view.php

--- www/views/logged-in-login-view.php
+++ www/views/logged-in-login-view.php
@@ -1,36 +1,26 @@
 <?php
 
-namespace LoginApp\View;
-
-use LoginApp\Model\LoginModel;
-
 class LoggedInLoginView extends LoginView {
 
-    private $getLogoutKey;
+	private $getLogoutKey;
 
-    public function __construct(LoginModel $model) {
-        parent::__construct($model);
+	public function __construct(LoginModel $model) {
+		parent::__construct($model);
 
-        $this->getLogoutKey = 'logout';
-        $this->headHtml = new HeadHtml('1DV408 - Logged in');
-    }
+		$this->getLogoutKey = 'logout';
+		$this->headHtml = new HeadHtml('1DV408 - Logged in');
+	}
 
-    public function wasLogoutLinkClicked() {
-        return (isset($_GET[$this->getLogoutKey]));
-    }
+	public function wasLogoutLinkClicked() {
+		return (isset($_GET[$this->getLogoutKey]));
+	}
 
-    public function renderPage() {
-        echo '<html>'
-        . $this->headHtml->getHtml() .
-        '<body>
-            <h1>Laborationskod hl222ih</h1>
-            <h2>' . $this->model->getUsername() . ' är inloggad' . '</h2>
-            ' . ($this->model->getNotification() ? '<p>' . $this->model->getNotification() . '</p>' : '') . '
-            <p><a href="?logout" onclick="">Logga ut</a></p>
-            <p></p>' .
-            $this->footerHtml->getHtml() .
-        '</body>
-        </html>';
-    }
-
+	public function renderPage() {
+		echo	$this->headHtml->getHtml() .
+				'<h1>Laborationskod hl222ih/ib222dp</h1>
+				<h2>' . $this->model->getUsername() . ' är inloggad' . '</h2>
+				' . ($this->model->getNotification() ? '<p>' . $this->model->getNotification() . '</p>' : '') . '
+				<p><a href="?logout" onclick="">Logga ut</a></p>'
+				. $this->footerHtml->getHtml();
+	}
 }

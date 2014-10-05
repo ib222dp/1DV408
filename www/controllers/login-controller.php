5e24239fecd0fe6549114b42c97e22fd45f781c0
ib222dp <ib222dp@users.noreply.github.com>
2014-10-05 16:56:41 +0200

    Update login-controller.php

--- www/controllers/login-controller.php
+++ www/controllers/login-controller.php
@@ -1,7 +1,4 @@
 <?php
-namespace LoginApp\Controller;
-use LoginApp\View as View;
-use LoginApp\Model as Model;
 
 require_once("models/login-model.php");
 require_once("views/login-view.php");
@@ -9,51 +6,54 @@ require_once("views/login-login-view.php");
 require_once("views/logged-in-login-view.php");
 
 class LoginController {
-    private $view;
-    private $model;
+	private $view;
+	private $model;
 
-    public function __construct() {
-        $this->model = new Model\LoginModel();
-    }
+	public function __construct() {
+		$this->model = new LoginModel();
+	}
 
-    public function start() {
-        if ($this->model->isLoggedIn()) {
-            $this->view = new View\LoggedInLoginView($this->model);
-            if (!$this->model->isSessionIntegrityOk()) {
-                session_regenerate_id(false);
-                $this->model->logout();
-                $this->model->unsetNotification(); //don't show a message to session hijacker that the hijacked session was logged out.
-                $this->view->redirectPage();
-            } elseif ($this->view->wasLogoutLinkClicked()) {
-                $this->model->logout();
-                $this->view->clearCookies();
-                $this->view->redirectPage();
-            } else {
-                $this->view->renderPage();
-                $this->model->unsetNotification();
-            }
-        } else {
-            $this->view = new View\LoginLoginView($this->model);
-            $willRedirect = false;
-            if ($this->view->wasLoginButtonClicked()) {
-                if ($this->model->tryLogin($this->view->getUsername(), $this->view->getPassword(), $this->view->wasAutoLoginChecked())) {
-                    $this->view->setCookiesIfAutoLogin();
-                    $this->model->setServersideCookieExpirationIfAutoLogin();
-                }
-                $willRedirect = true;
-            } elseif ($this->view->doesLoginCookieExist()) {
-                if ($this->model->tryLoginWithCookie($this->view->getUsernameFromCookie(), $this->view->getEncryptedPasswordFromCookie())) {
-                    $willRedirect = true;
-                } else {
-                    $this->view->clearCookies();
-                }
-            }
-            if ($willRedirect) {
-                $this->view->redirectPage();
-            } else {
-                $this->view->renderPage();
-                $this->model->unsetNotification();
-            }
-        }
-    }
+	public function start() {
+		$this->view = new LoginLoginView($this->model);
+		//Visar login-formulär om ny användare har registrerats
+		if($this->view->wasNewUserRegistered()){
+			$this->view->renderLoginPage();
+		}elseif ($this->model->isLoggedIn()) {
+			$this->view = new LoggedInLoginView($this->model);
+			if (!$this->model->isSessionIntegrityOk()) {
+				session_regenerate_id(false);
+				$this->model->logout();
+				$this->model->unsetNotification(); //don't show a message to session hijacker that the hijacked session was logged out.
+				$this->view->redirectPage();
+			} elseif ($this->view->wasLogoutLinkClicked()) {
+				$this->model->logout();
+				$this->view->clearCookies();
+				$this->view->redirectPage();
+			} else {
+				$this->view->renderPage();
+				$this->model->unsetNotification();
+			}
+		} else {
+			$this->view = new LoginLoginView($this->model);
+			$willRedirect = false;
+			if ($this->view->wasLoginButtonClicked()) {
+				if ($this->model->tryLogin($this->view->getUsername(), $this->view->getPassword(), $this->view->wasAutoLoginChecked())) {
+					$this->view->setCookiesIfAutoLogin();
+					$this->model->setServersideCookieExpirationIfAutoLogin();
+				}
+				$willRedirect = true;
+			} elseif ($this->view->doesLoginCookieExist()) {
+				if ($this->model->tryLoginWithCookie($this->view->getUsernameFromCookie(), $this->view->getEncryptedPasswordFromCookie())) {
+					$willRedirect = true;
+				} else {
+					$this->view->clearCookies();
+				}
+			} if ($willRedirect) {
+				$this->view->redirectPage();
+			} else {
+				$this->view->renderPage();
+				$this->model->unsetNotification();
+			}
+		}
+	}
 }

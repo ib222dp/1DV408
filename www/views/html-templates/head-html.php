bc02c0eefd3eb00e82fa2fe7b96550c1fea0409c
ib222dp <ib222dp@users.noreply.github.com>
2014-10-05 17:03:00 +0200

    Update head-html.php

--- www/views/html-templates/head-html.php
+++ www/views/html-templates/head-html.php
@@ -1,22 +1,20 @@
 <?php
 
-namespace LoginApp\View;
-
 class HeadHtml {
-    private $title;
+	private $title;
 
-    public function __construct($title) {
-        $this->title = $title;
-    }
+	public function __construct($title) {
+		$this->title = $title;
+	}
 
-    public function getHtml() {
-
-        return '
-        <head>
-            <meta charset="utf-8" />
-            <title>' . $this->title . '</title>
-            <link rel="stylesheet" type="text/css" href="styles/styles.css" />
-            <!-- <script src="scripts/login-app.js"></script> -->
-        </head>';
-    }
+	public function getHtml() {
+		return	'<!DOCTYPE html>
+				<html>
+				<head>
+					<title>' . $this->title . '</title>
+					<meta charset="utf-8" /> 
+					<link rel="stylesheet" type="text/css" href="styles/styles.css" />
+				</head>
+				<body>';
+	}
 }

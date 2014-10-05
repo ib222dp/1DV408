d06ab684575ab72372f3a8e724428c3241495555
ib222dp <ib222dp@users.noreply.github.com>
2014-10-05 17:01:52 +0200

    Update footer-html.php

--- www/views/html-templates/footer-html.php
+++ www/views/html-templates/footer-html.php
@@ -1,16 +1,21 @@
 <?php
-namespace LoginApp\View;
 
 class FooterHtml {
 
-    public function __construct() {
-    }
+	public function __construct() {
+		
+	}
 
-    public function getHtml() {
+	public function getHtml() {
+		//Sätter datum och tid
+		$day = utf8_encode(ucfirst(strftime("%A")));
+		$month = ucfirst(strftime("%B"));
+		$time = strftime("<p> " . $day . ", den %e " . $month . " år %Y. Klockan är [%T].</p>");
 
-        $dateStr = sprintf("%s den %d %s år %d. Klockan är [%s:%s:%s].", ucFirst(strftime("%A")) , date("j"), lcFirst(strftime("%B")), date("Y"), date("H"), date("i"), date("s"));
-        return '<footer>
-            ' . $dateStr . '
-        </footer>';
-    }
+		return	'<footer>
+					' . $time . '
+				</footer>
+				</body>
+				</html>';
+	}
 }
